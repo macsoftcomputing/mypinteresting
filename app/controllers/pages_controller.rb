5a43f4ba52c8eca4fdb0add14b09d00e9a1f6010
RJ McNair <robert@macsoftcomputing.com>
2015-05-05 09:28:46 -0400

    Created home and about

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController #first and last line define the file/
+  def home #def stands for define and home page
+   end
+   def about #def stands for define and about page 
+   end
+end
